cb133134f61a728a323edc982e504e2f5a56a59b
samsung <goodboy007@gmail.com>
2025-10-14 19:11:01 -0400

    day 1

---
+++ src/main/java/com/aksbscraperai/aksbscraperai/AksbscraperAiApplication.java
@@ -0,0 +1,13 @@
+package com.aksbscraperai.aksbscraperai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AksbscraperAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AksbscraperAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aksbscraperai/aksbscraperai/AksbscraperAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksbscraperai.aksbscraperai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AksbscraperAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
